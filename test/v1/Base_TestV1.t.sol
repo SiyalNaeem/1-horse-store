e6936166f4f75f197f0b9688967cf5dbdedf5346
Naeem Ali <siyalnaeem1997@gmail.com>
2025-06-11 00:14:48 +0100

    Add contract written in .huff and .yul and add tests

--- test/v1/Base_TestV1.t.sol
+++ test/v1/Base_TestV1.t.sol
@@ -3,17 +3,25 @@ pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
 import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
+import {IHorseStore} from "../../src/horseStoreV1/IHorseStore.sol";
 
 abstract contract Base_TestV1 is Test {
-    HorseStore public horseStore;
+    IHorseStore public horseStore;
 
     function setUp() public virtual {
-        horseStore = new HorseStore();
+        horseStore = IHorseStore(address(new HorseStore()));
     }
 
     function testReadValue() public view {
         uint256 initialValue = horseStore.readNumberOfHorses();
         console.log("Initial number of horses:", initialValue);
         assertEq(initialValue, 0);
+    }
+
+    function testWriteValue(uint256 numberOfHorses) public {
+        horseStore.updateHorseNumber(numberOfHorses);
+        uint256 updatedValue = horseStore.readNumberOfHorses();
+        console.log("Updated number of horses:", updatedValue);
+        assertEq(updatedValue, numberOfHorses);
     }
 }
--- test/v1/HorseStoreSolc.t.sol
+++ test/v1/HorseStoreSolc.t.sol
@@ -1,10 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
-import {HorseStore} from "../../src/horseStoreV1/HorseStore.sol";
 import {Base_TestV1} from "./Base_TestV1.t.sol";
 
-contract HorseStoreSolc is Base_TestV1 {
-
-}
+contract HorseStoreSolc is Base_TestV1 {}
